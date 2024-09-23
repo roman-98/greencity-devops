4520e688710c32149ed2ff4f1d82e4a83f470fc1
Roman Strypa <romanstripa@gmail.com>
2024-09-23 21:54:00 +0000

    add-rds

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -7,26 +7,24 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "private" {
-  count = length(var.azs)
-
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index)
-  availability_zone = var.azs[count.index]
+  count                   = length(var.private_subnets)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = element(var.private_subnets, count.index)
+  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
 
   tags = {
-    Name = "${var.cluster_name}-private-${count.index}"
+    Name = "${var.cluster_name}-private-${count.index + 1}"
   }
 }
 
 resource "aws_subnet" "public" {
-  count = length(var.azs)
-
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index + 1)
-  availability_zone = var.azs[count.index]
+  count                   = length(var.public_subnets)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = element(var.public_subnets, count.index)
+  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
 
   tags = {
-    Name = "${var.cluster_name}-public-${count.index}"
+    Name = "${var.cluster_name}-public-${count.index + 1}"
   }
 }
 
--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -12,3 +12,15 @@ variable "cluster_name" {
   description = "Назва кластера EKS"
   type        = string
 }
+
+variable "private_subnets" {
+  description = "List of private subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]  # Залиште CIDR-адреси або змініть їх на інші, якщо потрібно
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]  # Змінено CIDR-адреси для уникнення конфліктів
+}
