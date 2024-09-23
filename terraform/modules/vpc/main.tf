10e47096a980439c11f638b9c0f8d628dc484456
Roman Strypa <romanstripa@gmail.com>
2024-09-23 21:56:50 +0000

    add-rds

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,16 +1,18 @@
+data "aws_availability_zones" "available" {}
+
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
 
   tags = {
-    Name = "${var.cluster_name}-vpc"
+    Name = var.cluster_name
   }
 }
 
 resource "aws_subnet" "private" {
-  count                   = length(var.private_subnets)
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = element(var.private_subnets, count.index)
-  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
+  count             = length(var.private_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.private_subnets, count.index)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
 
   tags = {
     Name = "${var.cluster_name}-private-${count.index + 1}"
@@ -18,32 +20,12 @@ resource "aws_subnet" "private" {
 }
 
 resource "aws_subnet" "public" {
-  count                   = length(var.public_subnets)
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = element(var.public_subnets, count.index)
-  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
+  count             = length(var.public_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.public_subnets, count.index)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
 
   tags = {
     Name = "${var.cluster_name}-public-${count.index + 1}"
   }
-}
-
-resource "aws_security_group" "db" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "${var.cluster_name}-db-sg"
-  }
-}
-
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "private_subnets" {
-  value = aws_subnet.private[*].id
-}
-
-output "db_security_group_ids" {
-  value = [aws_security_group.db.id]
 }
