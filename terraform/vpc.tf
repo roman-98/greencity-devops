5588c9af04e48bffd6535813884687af91b26b67
Roman Strypa <romanstripa@gmail.com>
2024-09-24 12:50:36 +0000

    rds

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -50,17 +50,24 @@ resource "aws_security_group" "eks_sg" {
   vpc_id = aws_vpc.main.id
 
   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -75,14 +82,14 @@ resource "aws_security_group" "rds_sg" {
     from_port   = 5432  # для PostgreSQL, змініть на 3306 для MySQL
     to_port     = 5432
     protocol    = "tcp"
-    security_groups = [aws_security_group.eks_sg.id]
+    cidr_blocks = ["10.0.2.0/24", "10.0.4.0/24"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["10.0.0.0/16", "10.0.2.0/24", "10.0.4.0/24"]
   }
 
   tags = {
