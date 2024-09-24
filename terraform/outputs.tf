4334fae3119e59a11b24ac1d2bbee049b06ddad0
Roman Strypa <romanstripa@gmail.com>
2024-09-24 00:27:15 +0000

    rds

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -14,14 +14,5 @@ output "region" {
   value = var.aws_region
 }
 
-output "rds_endpoint" {
-  description = "PostgreSQL RDS endpoint"
-  value       = module.rds.db_instance_endpoint
-}
 
-output "rds_password" {
-  description = "Пароль для PostgreSQL RDS"
-  value       = random_password.rds_password.result
-  sensitive   = true
-}
 
