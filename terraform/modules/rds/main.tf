81fb44567ed0ccc1a1b9de8409891d2a65dc252e
Roman Strypa <romanstripa@gmail.com>
2024-09-24 08:56:33 +0000

    new-infra

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -24,7 +24,7 @@ resource "aws_security_group" "rds_sg" {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = [module.vpc.cidr_block]
+    cidr_blocks = module.vpc.cidr_block
   }
 
   egress {
