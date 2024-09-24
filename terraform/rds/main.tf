3386b618b0e8f87b1acada9334c59542574c6e2f
Roman Strypa <romanstripa@gmail.com>
2024-09-24 01:24:05 +0000

    rds

--- terraform/rds/main.tf
+++ terraform/rds/main.tf
@@ -1,48 +1,25 @@
-resource "aws_db_subnet_group" "default" {
-  name       = "${var.cluster_name}-rds-subnet-group"
-  subnet_ids = var.subnet_ids
+module "rds" {
+  source  = "terraform-aws-modules/rds/aws"
+  version = "5.6.0"
+
+  identifier         = "my-postgres-db"
+  engine             = "postgres"
+  engine_version     = "13.4"
+  instance_class     = "db.t3.medium"
+  allocated_storage  = 20
+  storage_type       = "gp2"
+  username           = "admin"
+  password           = random_password.rds_password.result
+  multi_az           = true
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+
+  family = "postgres13"  
+
+  # Підмережі для Multi-AZ
+  subnet_ids = module.vpc.private_subnets
 
   tags = {
-    Name = "${var.cluster_name}-rds-subnet-group"
-  }
-}
-
-resource "aws_rds_cluster" "postgresql" {
-  cluster_identifier      = "${var.cluster_name}-rds-cluster"
-  engine                  = "aurora-postgresql"
-  engine_version          = "14.5"
-  availability_zones      = var.azs
-  database_name           = var.db_name
-  master_username         = var.db_username
-  master_password         = var.db_password
-  backup_retention_period = 5
-  preferred_backup_window = "07:00-09:00"
-  db_subnet_group_name    = aws_db_subnet_group.default.name
-  skip_final_snapshot     = true
-
-  vpc_security_group_ids = [var.security_group_id]
-
-  tags = {
-    Name = "${var.cluster_name}-rds-cluster"
-  }
-}
-
-resource "aws_rds_cluster_instance" "cluster_instances" {
-  count              = 2
-  identifier         = "${var.cluster_name}-rds-instance-${count.index}"
-  cluster_identifier = aws_rds_cluster.postgresql.id
-  instance_class     = "db.r5.large"
-  engine             = aws_rds_cluster.postgresql.engine
-  engine_version     = aws_rds_cluster.postgresql.engine_version
-}
-
-resource "aws_db_parameter_group" "postgresql" {
-  family = "aurora-postgresql14"
-  name   = "${var.cluster_name}-db-parameter-group"
-
-  parameter {
-    name  = "log_connections"
-    value = "1"
+    Name = "my-postgres-db"
   }
 }
 
--- terraform/rds/variables.tf
+++ terraform/rds/variables.tf
@@ -3,40 +3,7 @@ variable "db_parameter_group_name" {
   type        = string
 }
 
-variable "cluster_name" {
-  description = "The name of the cluster"
-  type        = string
-}
-
-variable "subnet_ids" {
-  description = "A list of subnet IDs to launch the cluster in"
-  type        = list(string)
-}
-
-variable "azs" {
-  description = "A list of availability zones in the region"
-  type        = list(string)
-}
-
-variable "db_name" {
-  description = "Name for the database"
-  type        = string
-  default = "greencity"
-}
-
-variable "db_username" {
-  description = "Username for the database"
-  type        = string
-  default = "postgres"
-}
-
-variable "db_password" {
-  description = "Password for the database"
-  type        = string
-  default = "password"
-}
-
-variable "security_group_id" {
-  description = "The ID of the security group to associate with the RDS cluster"
+variable "family" {
+  description = "The DB parameter group family"
   type        = string
 }
