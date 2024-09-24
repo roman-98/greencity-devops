a3a8357477a88a6de46f82f0c297255cad238719
Roman Strypa <romanstripa@gmail.com>
2024-09-24 17:14:27 +0000

    add autoscaling

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -78,8 +78,10 @@ resource "aws_iam_role" "eks_node_role" {
       Action = "sts:AssumeRole"
       Effect = "Allow"
       Principal = {
-        Service = "ec2.amazonaws.com"
-      }
+        Service = [
+            "ec2.amazonaws.com",
+            "eks.amazonaws.com"
+      ]}
     }]
     Version = "2012-10-17"
   })
