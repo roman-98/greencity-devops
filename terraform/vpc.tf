553f313aafdaec12e05754e12a1cf72b21bf1bfc
Roman Strypa <romanstripa@gmail.com>
2024-09-24 13:28:42 +0000

    current configuration

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -6,26 +6,6 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "public_subnet_a" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "public_subnet_a"
-  }
-}
-
-resource "aws_subnet" "public_subnet_b" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "public_subnet_b"
-  }
-}
-
 resource "aws_subnet" "private_subnet_a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
