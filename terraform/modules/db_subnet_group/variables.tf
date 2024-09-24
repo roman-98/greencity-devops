2763f1d115058e51c2385edc479eb630f0b46053
Roman Strypa <romanstripa@gmail.com>
2024-09-24 01:11:11 +0000

    rds

--- terraform/modules/db_subnet_group/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "name" {
-  description = "Назва групи підмереж"
-  type        = string
-}
-
-variable "subnet_ids" {
-  description = "Список ID підмереж для групи підмереж"
-  type        = list(string)
-}
