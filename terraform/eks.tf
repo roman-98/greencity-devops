f440c81941cc38274efd4aabde94698a29eed54c
Roman Strypa <romanstripa@gmail.com>
2024-09-24 15:37:20 +0000

    add autoscaling

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -38,3 +38,63 @@ resource "aws_iam_role_policy_attachment" "eks_AmazonEKSServicePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
   role       = aws_iam_role.eks_role.name
 }
+
+resource "aws_eks_node_group" "my_eks_node_group" {
+  cluster_name    = aws_eks_cluster.my_cluster.name
+  node_group_name = "my-eks-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = [aws_subnet.private_subnet_a.id, aws_subnet.private_subnet_b.id]
+  
+  scaling_config {
+    desired_size = 2  # Кількість нод за замовчуванням
+    max_size     = 5  # Максимальна кількість нод для автоскейлінгу
+    min_size     = 1  # Мінімальна кількість нод для автоскейлінгу
+  }
+
+  instance_types = ["t3.medium"]  # Тип інстансів для воркер-нод
+  
+  ami_type = "AL2_x86_64"  # Amazon Linux 2 для x86 архітектури
+
+#  remote_access {
+#    ec2_ssh_key = "my-key-pair"  # SSH ключ для доступу до EC2 інстансів
+#  }
+
+  tags = {
+    Name = "my_eks_node_group"
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_AmazonEKSWorkerNodePolicy]
+}
+
+# IAM роль для воркер-нод
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks_node_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Effect    = "Allow"
+      Sid       = ""
+    }]
+  })
+}
+
+# Права доступу для воркер-нод
+resource "aws_iam_role_policy_attachment" "eks_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_role.name
+}
