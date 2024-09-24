5de5fd3761b563657bd9486a35fee95127564d73
Roman Strypa <romanstripa@gmail.com>
2024-09-24 16:54:29 +0000

    add autoscaling

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -13,21 +13,6 @@ resource "aws_eks_cluster" "my_cluster" {
   depends_on = [aws_iam_role_policy_attachment.eks_AmazonEKSClusterPolicy]
 }
 
-resource "aws_iam_role" "eks_node_role" {
-  name = "eks_node_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-  })
-}
-
 resource "aws_iam_role_policy_attachment" "eks_AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.eks_role.name
