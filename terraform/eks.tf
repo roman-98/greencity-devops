990b86cd41519e8bfb24a0284ebc1b2fc02c1b28
Roman Strypa <romanstripa@gmail.com>
2024-09-25 09:09:29 +0000

    new-infa

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,7 +1,8 @@
+data "aws_caller_identity" "current" {}
+
 resource "aws_eks_cluster" "my_cluster" {
   name     = "my-cluster"
   role_arn = aws_iam_role.eks_role.arn
-
   vpc_config {
     subnet_ids = [
       aws_subnet.public_subnet_a.id,
@@ -9,22 +10,19 @@ resource "aws_eks_cluster" "my_cluster" {
     ]
     security_group_ids = [aws_security_group.eks_sg.id]
   }
-
   depends_on = [aws_iam_role_policy_attachment.eks_AmazonEKSClusterPolicy]
 }
 
 resource "aws_iam_role" "eks_role" {
   name = "eks_role"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
       Action = "sts:AssumeRole"
       Effect = "Allow"
       Principal = {
-        Service = [
-            "eks.amazonaws.com"
-      ]}
+        Service = "eks.amazonaws.com"
+      }
     }]
   })
 }
@@ -41,7 +39,6 @@ resource "aws_iam_role_policy_attachment" "eks_AmazonEKSServicePolicy" {
 
 resource "aws_iam_role" "eks_node_role" {
   name = "eks_node_role"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
@@ -49,11 +46,6 @@ resource "aws_iam_role" "eks_node_role" {
       Effect = "Allow"
       Principal = {
         Service = "ec2.amazonaws.com"
-      }
-      Condition = {
-        StringEquals = {
-          "sts:ExternalId" = "eks:${aws_eks_cluster.my_cluster.name}"
-        }
       }
     }]
   })
@@ -74,20 +66,29 @@ resource "aws_iam_role_policy_attachment" "eks_registry_policy_attachment" {
   role       = aws_iam_role.eks_node_role.name
 }
 
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.my_cluster.identity[0].oidc[0].issuer
+}
+
+data "tls_certificate" "eks" {
+  url = aws_eks_cluster.my_cluster.identity[0].oidc[0].issuer
+}
+
 resource "aws_iam_role" "vpc_cni_irsa_role" {
   name = "VPC-CNI-IRSA"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action = "sts:AssumeRole"
+      Action = "sts:AssumeRoleWithWebIdentity"
       Effect = "Allow"
       Principal = {
-        Federated = aws_eks_cluster.my_cluster.identity[0].oidc[0].issuer
+        Federated = aws_iam_openid_connect_provider.eks.arn
       }
       Condition = {
         StringEquals = {
-          "${aws_eks_cluster.my_cluster.identity[0].oidc[0].issuer}:sub" = "system:serviceaccount:kube-system:aws-node"
+          "${replace(aws_eks_cluster.my_cluster.identity[0].oidc[0].issuer, "https://", "")}:sub" = "system:serviceaccount:kube-system:aws-node"
         }
       }
     }]
@@ -104,12 +105,41 @@ module "vpc_cni_irsa" {
 
   oidc_providers = {
     main = {
-      provider_arn               = aws_eks_cluster.my_cluster.identity[0].oidc[0].issuer,
+      provider_arn               = aws_iam_openid_connect_provider.eks.arn
       namespace_service_accounts = ["kube-system:aws-node"]
     }
   }
 
   depends_on = [aws_eks_cluster.my_cluster]
+}
+
+resource "aws_eks_node_group" "my_node_group" {
+  cluster_name    = aws_eks_cluster.my_cluster.name
+  node_group_name = "my-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = [aws_subnet.public_subnet_a.id, aws_subnet.public_subnet_b.id]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_node_policy_attachment,
+    aws_iam_role_policy_attachment.eks_cni_policy_attachment,
+    aws_iam_role_policy_attachment.eks_registry_policy_attachment,
+  ]
+}
+
+provider "kubectl" {
+  host                   = aws_eks_cluster.my_cluster.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.my_cluster.certificate_authority[0].data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.my_cluster.name]
+    command     = "aws"
+  }
 }
 
 resource "kubectl_manifest" "aws_auth" {
@@ -131,8 +161,7 @@ data:
         - system:bootstrappers
         - system:nodes
 EOF
-
-  depends_on = [aws_eks_cluster.my_cluster]
+  depends_on = [aws_eks_cluster.my_cluster, aws_eks_node_group.my_node_group]
 }
 
 resource "kubectl_manifest" "database_secret" {
@@ -141,12 +170,11 @@ apiVersion: v1
 kind: Secret
 metadata:
   name: db-secret
-  namespace: default  # Змініть на ваш неймспейс
+  namespace: default
 type: Opaque
 data:
-  username: ${base64encode("your-db-username")}  # Змініть на ваше ім'я користувача
-  password: ${base64encode("your-db-password")}  # Змініть на ваш пароль
+  username: ${base64encode("your-db-username")}
+  password: ${base64encode("your-db-password")}
 EOF
-
   depends_on = [kubectl_manifest.aws_auth]
 }
