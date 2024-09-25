6517f506a9b32df62d2fff2aa42484f2b55add57
Roman Strypa <romanstripa@gmail.com>
2024-09-25 08:45:12 +0000

    new-infa

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,18 +1,3 @@
-resource "aws_eks_cluster" "my_cluster" {
-  name     = "my-cluster"
-  role_arn = aws_iam_role.eks_role.arn
-
-  vpc_config {
-    subnet_ids = [
-      aws_subnet.public_subnet_a.id,
-      aws_subnet.public_subnet_b.id
-    ]
-    security_group_ids = [aws_security_group.eks_sg.id]
-  }
-
-  depends_on = [aws_iam_role_policy_attachment.eks_AmazonEKSClusterPolicy]
-}
-
 resource "aws_iam_role" "eks_role" {
   name = "eks_role"
 
@@ -28,6 +13,34 @@ resource "aws_iam_role" "eks_role" {
       ]}
     }]
   })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks_role.name
+}
+
+resource "aws_eks_cluster" "my_cluster" {
+  name     = "my-cluster"
+  role_arn = aws_iam_role.eks_role.arn
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.public_subnet_a.id,
+      aws_subnet.public_subnet_b.id
+    ]
+    security_group_ids = [aws_security_group.eks_sg.id]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.eks_AmazonEKSServicePolicy
+  ]
 }
 
 resource "aws_iam_role" "vpc_cni_irsa_role" {
