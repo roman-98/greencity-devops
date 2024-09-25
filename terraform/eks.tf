9e0ecdd0991c70842f58f35a398c127de3893ea8
Roman Strypa <romanstripa@gmail.com>
2024-09-25 11:06:16 +0000

    new-infa

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,8 +1,72 @@
+# eks.tf
+
+# Data source to get the current AWS account ID
 data "aws_caller_identity" "current" {}
 
+# IAM Role for EKS Cluster
+resource "aws_iam_role" "eks_role" {
+  name = "eks_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# IAM Role for EKS Nodes
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks_node_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# IAM Role for VPC CNI with IRSA (IAM Roles for Service Accounts)
+resource "aws_iam_role" "vpc_cni_irsa_role" {
+  name = "VPC-CNI-IRSA"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_eks_cluster.my_cluster.identity[0].oidc.issuer
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${aws_eks_cluster.my_cluster.identity[0].oidc.issuer}:sub" = "system:serviceaccount:kube-system:aws-node"
+          }
+        }
+      }
+    ]
+  })
+}
+
+# EKS Cluster
 resource "aws_eks_cluster" "my_cluster" {
   name     = "my-cluster"
   role_arn = aws_iam_role.eks_role.arn
+
   vpc_config {
     subnet_ids = [
       aws_subnet.public_subnet_a.id,
@@ -10,28 +74,15 @@ resource "aws_eks_cluster" "my_cluster" {
     ]
     security_group_ids = [aws_security_group.eks_sg.id]
   }
-  depends_on = [aws_iam_role_policy_attachment.eks_AmazonEKSClusterPolicy]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.eks_AmazonEKSServicePolicy,
+    aws_iam_role_policy.eks_create_cluster_policy
+  ]
 }
 
-resource "aws_iam_role" "eks_role" {
-  name = "eks_role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = [
-          "eks:*",
-          "sts:AssumeRole",
-          "iam:PassRole"
-      ]
-      Resource = "*"
-      Effect = "Allow"
-      Principal = {
-        Service = "eks.amazonaws.com"
-      }
-    }]
-  })
-}
-
+# IAM Policies for EKS Role
 resource "aws_iam_role_policy_attachment" "eks_AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.eks_role.name
@@ -42,92 +93,47 @@ resource "aws_iam_role_policy_attachment" "eks_AmazonEKSServicePolicy" {
   role       = aws_iam_role.eks_role.name
 }
 
-resource "aws_iam_role" "eks_node_role" {
-  name = "eks_node_role"
-  assume_role_policy = jsonencode({
+# IAM Policy Attachment for EKS Create Cluster Permission
+resource "aws_iam_role_policy" "eks_create_cluster_policy" {
+  name = "eks-create-cluster-policy"
+  role = aws_iam_role.eks_role.name
+
+  policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Action = [
-          "eks:*",
-          "sts:AssumeRole",
-          "iam:PassRole"
-      ]
-      Resource = "*"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "eks_node_policy_attachment" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.eks_node_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "eks_cni_policy_attachment" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.eks_node_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "eks_registry_policy_attachment" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.eks_node_role.name
-}
-
-resource "aws_iam_openid_connect_provider" "eks" {
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
-  url             = aws_eks_cluster.my_cluster.identity[0].oidc[0].issuer
-}
-
-data "tls_certificate" "eks" {
-  url = aws_eks_cluster.my_cluster.identity[0].oidc[0].issuer
-}
-
-resource "aws_iam_role" "vpc_cni_irsa_role" {
-  name = "VPC-CNI-IRSA"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRoleWithWebIdentity"
-      Effect = "Allow"
-      Principal = {
-        Federated = aws_iam_openid_connect_provider.eks.arn
-      }
-      Condition = {
-        StringEquals = {
-          "${replace(aws_eks_cluster.my_cluster.identity[0].oidc[0].issuer, "https://", "")}:sub" = "system:serviceaccount:kube-system:aws-node"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "eks:CreateCluster"
+        Resource = "arn:aws:eks:us-west-2:${data.aws_caller_identity.current.account_id}:cluster/my-cluster"
+      },
+      {
+        Effect = "Allow"
+        Action = "iam:CreateServiceLinkedRole"
+        Resource = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/eks.amazonaws.com/AWSServiceRoleForAmazonEKS"
+        Condition = {
+          "ForAnyValue:StringEquals" = {
+            "iam:AWSServiceName" = "eks"
+          }
         }
+      },
+      {
+        Effect = "Allow"
+        Action = "iam:PassRole"
+        Resource = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${aws_iam_role.eks_role.name}"
       }
-    }]
+    ]
   })
 }
 
-module "vpc_cni_irsa" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "~> 4.12"
-
-  role_name_prefix      = "VPC-CNI-IRSA"
-  attach_vpc_cni_policy = true
-  vpc_cni_enable_ipv4   = true
-
-  oidc_providers = {
-    main = {
-      provider_arn               = aws_iam_openid_connect_provider.eks.arn
-      namespace_service_accounts = ["kube-system:aws-node"]
-    }
-  }
-
-  depends_on = [aws_eks_cluster.my_cluster]
-}
-
+# EKS Managed Node Group
 resource "aws_eks_node_group" "my_node_group" {
   cluster_name    = aws_eks_cluster.my_cluster.name
   node_group_name = "my-node-group"
   node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = [aws_subnet.public_subnet_a.id, aws_subnet.public_subnet_b.id]
+  subnet_ids      = [
+    aws_subnet.public_subnet_a.id,
+    aws_subnet.public_subnet_b.id
+  ]
 
   scaling_config {
     desired_size = 2
@@ -136,24 +142,32 @@ resource "aws_eks_node_group" "my_node_group" {
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.eks_node_policy_attachment,
-    aws_iam_role_policy_attachment.eks_cni_policy_attachment,
-    aws_iam_role_policy_attachment.eks_registry_policy_attachment,
+    aws_eks_cluster.my_cluster,
+    aws_iam_role_policy_attachment.eks_NodePolicy,
+    aws_iam_role_policy_attachment.eks_CNI_Policy,
+    aws_iam_role_policy_attachment.eks_RegistryPolicy
   ]
 }
 
-provider "kubectl" {
-  host                   = aws_eks_cluster.my_cluster.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.my_cluster.certificate_authority[0].data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.my_cluster.name]
-    command     = "aws"
-  }
+# IAM Policies for EKS Node Group
+resource "aws_iam_role_policy_attachment" "eks_NodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_role.name
 }
 
+resource "aws_iam_role_policy_attachment" "eks_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_RegistryPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+# Kubernetes ConfigMap for aws-auth
 resource "kubectl_manifest" "aws_auth" {
-  yaml_body = <<EOF
+  yaml_body = <<EOT
 apiVersion: v1
 kind: ConfigMap
 metadata:
@@ -161,30 +175,18 @@ metadata:
   namespace: kube-system
 data:
   mapRoles: |
-    - rolearn: ${aws_iam_role.eks_role.arn}
-      username: admin
-      groups:
-        - system:masters
     - rolearn: ${aws_iam_role.eks_node_role.arn}
       username: system:node:{{EC2PrivateDNSName}}
       groups:
         - system:bootstrappers
         - system:nodes
-EOF
-  depends_on = [aws_eks_cluster.my_cluster, aws_eks_node_group.my_node_group]
-}
+    - rolearn: ${aws_iam_role.eks_role.arn}
+      username: admin
+      groups:
+        - system:masters
+EOT
 
-resource "kubectl_manifest" "database_secret" {
-  yaml_body = <<EOF
-apiVersion: v1
-kind: Secret
-metadata:
-  name: db-secret
-  namespace: default
-type: Opaque
-data:
-  username: ${base64encode("your-db-username")}
-  password: ${base64encode("your-db-password")}
-EOF
-  depends_on = [kubectl_manifest.aws_auth]
+  depends_on = [
+    aws_eks_cluster.my_cluster
+  ]
 }
