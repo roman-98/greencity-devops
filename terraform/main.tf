98f4b94eba4064295d3753541af8e7717ec7a64b
Roman Strypa <romanstripa@gmail.com>
2024-09-26 11:54:20 +0000

    terraform-updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,8 +5,22 @@ module "vpc" {
 module "rds" {
   source = "./modules/rds"
 
+  username             = var.username  
+  password             = var.password 
+
   vpc_security_group_ids = module.vpc.rds_security_group_id
   subnet_group_name      = module.vpc.rds_subnet_group_name
   subnet_ids             = [module.vpc.private_subnet_a_id, module.vpc.private_subnet_b_id]
 
+}
+
+variable "username" {
+  description = "The master username for the DB instance."
+  type        = string
+}
+
+variable "password" {
+  description = "The master password for the DB instance."
+  type        = string
+  sensitive   = true
 }
