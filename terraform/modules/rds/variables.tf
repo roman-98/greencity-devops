11ab70ae1140be4e384082046b7050f41dd8b66c
Roman Strypa <romanstripa@gmail.com>
2024-09-26 03:07:58 +0000

    terraform-updated

--- terraform/modules/rds/variables.tf
+++ terraform/modules/rds/variables.tf
@@ -72,12 +72,6 @@ variable "skip_final_snapshot" {
   default     = true
 }
 
-variable "multi_az" {
-  description = "Whether to enable Multi-AZ support for the DB instance."
-  type        = bool
-  default     = true
-}
-
 variable "tags" {
   description = "A map of tags to apply to the DB instance."
   type        = map(string)
