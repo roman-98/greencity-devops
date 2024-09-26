4c9db994eaf14f64a954b98c27c50784283ebe6e
Roman Strypa <romanstripa@gmail.com>
2024-09-26 00:22:25 +0000

    terraform-new

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -13,6 +13,27 @@ resource "aws_eks_cluster" "my_cluster" {
   depends_on = [aws_iam_role_policy_attachment.eks_AmazonEKSClusterPolicy]
 }
 
+resource "aws_eks_node_group" "eks_node_group" {
+  cluster_name    = aws_eks_cluster.my_cluster.name
+  node_group_name = "my-node-group"
+  node_role_arn   = aws_iam_role.eks_worker_role.arn
+  subnet_ids      = [
+    aws_subnet.private_subnet_a.id,
+    aws_subnet.private_subnet_b.id
+  ]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = ["t3.medium"]
+
+  depends_on = [aws_iam_role_policy_attachment.eks_AmazonEKSWorkerNodePolicy]
+}
+
+
 resource "aws_iam_role" "eks_role" {
   name = "eks_role"
 
@@ -27,6 +48,26 @@ resource "aws_iam_role" "eks_role" {
       Sid       = ""
     }]
   })
+}
+
+resource "aws_iam_role" "eks_worker_role" {
+  name = "eks_worker_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_worker_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "eks_AmazonEKSClusterPolicy" {
