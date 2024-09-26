ca3732004cf87d0d0fca4390b89ebfd8eeeff77b
Roman Strypa <romanstripa@gmail.com>
2024-09-26 10:31:49 +0000

    terraform-updated

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -1,7 +1,3 @@
-module "vpc" {
-  source = "../vpc"
-}
-
 resource "aws_db_instance" "greencity" {
   identifier              = var.identifier
   engine                  = var.engine
@@ -12,8 +8,6 @@ resource "aws_db_instance" "greencity" {
   username                = var.username
   password                = var.password
   db_name                 = var.db_name
-  vpc_security_group_ids  = module.vpc.rds_security_group_id
-  db_subnet_group_name    = module.vpc.rds_subnet_group_name
   skip_final_snapshot     = var.skip_final_snapshot
   multi_az                = var.multi_az
 
