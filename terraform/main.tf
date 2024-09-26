a20ade005e836cc78cc51a22d7582c5232bc134f
Roman Strypa <romanstripa@gmail.com>
2024-09-26 11:46:13 +0000

    terraform-updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,9 +4,6 @@ module "vpc" {
 
 module "rds" {
   source = "./modules/rds"
-  
-  username               = module.rds.var.username
-  password               = module.rds.var.password
 
   vpc_security_group_ids = module.vpc.rds_security_group_id
   subnet_group_name      = module.vpc.rds_subnet_group_name
