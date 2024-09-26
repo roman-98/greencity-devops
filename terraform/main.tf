26f99feef164c21163d4f3123c66cc7e8b8c077f
Roman Strypa <romanstripa@gmail.com>
2024-09-26 17:49:40 +0000

    terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ module "rds" {
 
   vpc_security_group_ids = module.vpc.rds_security_group_id
   subnet_group_name      = module.vpc.rds_subnet_group_name
-  subnet_ids             = [module.vpc.private_subnet_a_id]
+  subnet_ids             = module.vpc.private_subnet_a_id
 
 }
 
