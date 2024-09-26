c9b4aec55292c57cf769f4492077622959d7df5d
Roman Strypa <romanstripa@gmail.com>
2024-09-26 02:17:56 +0000

    terraform-updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ module "rds" {
   password               = "yourpassword"
   db_name                = "mydb"
 
-  vpc_security_group_ids = [module.vpc.rds_security_group_id]
+  vpc_security_group_ids = module.vpc.rds_security_group_id
   subnet_group_name      = module.vpc.rds_subnet_group_name
   subnet_ids             = [module.vpc.private_subnet_a_id, module.vpc.private_subnet_b_id]
 
