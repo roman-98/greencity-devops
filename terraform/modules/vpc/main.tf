c53d9e0a960a79e0ddde034d14b13b776e7ae4f1
Roman Strypa <romanstripa@gmail.com>
2024-10-01 19:09:23 +0000

    add-nat&igw

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -26,6 +26,91 @@ resource "aws_subnet" "private_subnet_b" {
   }
 }
 
+resource "aws_subnet" "public_subnet_a" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet_a_cidr
+  availability_zone       = var.availability_zone_a
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public_subnet_a"
+  }
+}
+
+resource "aws_subnet" "public_subnet_b" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet_b_cidr
+  availability_zone       = var.availability_zone_b
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public_subnet_b"
+  }
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+  tags = {
+    Name = "${var.vpc_name}-igw"
+  }
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+  tags = {
+    Name = "${var.vpc_name}-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "this" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_subnet_a.id
+  tags = {
+    Name = "${var.vpc_name}-nat-gw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+  tags = {
+    Name = "${var.vpc_name}-public-rt"
+  }
+}
+
+# Таблиця маршрутизації для приватних підмереж
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this.id
+  }
+  tags = {
+    Name = "${var.vpc_name}-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_subnet_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_subnet_b.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_subnet_a.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = aws_subnet.private_subnet_b.id
+  route_table_id = aws_route_table.private.id
+}
+
 resource "aws_security_group" "eks_sg" {
   vpc_id = aws_vpc.this.id
 
--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -22,6 +22,18 @@ variable "private_subnet_b_cidr" {
   default     = "10.0.4.0/24"
 }
 
+variable "public_subnet_a_cidr" {
+  description = "CIDR block for public subnet A."
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "public_subnet_b_cidr" {
+  description = "CIDR block for public subnet B."
+  type        = string
+  default     = "10.0.3.0/24"
+}
+
 variable "availability_zone_a" {
   description = "Availability zone for private subnet A."
   type        = string
