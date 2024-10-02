dd5058d922311bfee12f8006dc4f437e4e946176
Roman Strypa <romanstripa@gmail.com>
2024-10-02 22:13:18 +0100

    add-aws-sm

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -90,14 +90,12 @@ data "aws_eks_cluster_auth" "cluster" {
   name = aws_eks_cluster.main.name
 }
 
-# Configure the Kubernetes provider
 provider "kubernetes" {
   host                   = aws_eks_cluster.main.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
-# Configure the Helm provider
 provider "helm" {
   kubernetes {
     host                   = aws_eks_cluster.main.endpoint
