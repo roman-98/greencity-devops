37f71a46ff0f6d1d9a223a4aa4dde114ac69392a
Roman Strypa <romanstripa@gmail.com>
2024-10-04 20:49:38 +0100

    aws-sm&kubernetes

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -96,10 +96,6 @@ data "aws_eks_cluster_auth" "cluster" {
   name = aws_eks_cluster.main.name
 }
 
-output "cluster_endpoint" {
-  value       = aws_eks_cluster.main.endpoint
-}
-
 output "cluster_ca_certificate" {
   value       = aws_eks_cluster.main.certificate_authority[0].data
 }
