8137a7a3bceffde1049081f3aaab51f785e55c71
Roman Strypa <romanstripa@gmail.com>
2024-10-04 20:55:03 +0100

    aws-sm&kubernetes

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -87,8 +87,8 @@ resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
 }
 
 provider "kubernetes" {
-  host                   = aws_eks_cluster.main_endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority_data)
+  host                   = aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
