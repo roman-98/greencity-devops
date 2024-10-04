44350b9f5141407289ca970c5c316b21100f6227
Roman Strypa <romanstripa@gmail.com>
2024-10-04 15:42:52 +0100

    resource-secrets

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -81,10 +81,14 @@ resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
   role       = aws_iam_role.eks_nodes.name
 }
 
+data "tls_certificate" "eks" {
+  url = aws_eks_cluster.main.identity[0].oidc[0].issuer
+}
+
 resource "aws_iam_openid_connect_provider" "eks" {
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
-  url             = aws_eks_cluster.eks.identity[0].oidc[0].issuer
+  url             = aws_eks_cluster.main.identity[0].oidc[0].issuer
 }
 
 resource "aws_iam_role" "myapp_secrets" {
