79361c3e2d9b52547485096e20167634b5716599
Roman Strypa <romanstripa@gmail.com>
2024-10-06 11:08:21 +0100

    aws-sm&kubernetes

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,6 @@ module "rds" {
   ]
 
   depends_on = [module.vpc]
-
 }
 
 module "eks" {
@@ -25,20 +24,6 @@ module "eks" {
   private_subnet_a_id   = module.vpc.private_subnet_a_id
   private_subnet_b_id   = module.vpc.private_subnet_b_id
   eks_security_group_id = module.vpc.eks_security_group_id
-
-  depends_on = [module.rds]
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-      command     = "aws"
-    }
-  }
 }
 
 variable "username" {
