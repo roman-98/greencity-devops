0d194fd71c67c92b8ca8d6b724f6ea22bb8ffc87
Roman Strypa <romanstripa@gmail.com>
2024-10-31 16:32:29 +0000

    new-vpc-module

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -1,49 +1,17 @@
+output "private_subnet_ids" {
+  value = [
+    aws_subnet.private-eu-west-3a.id,
+    aws_subnet.private-eu-west-3b.id
+  ]
+}
+
+output "public_subnet_ids" {
+  value = [
+    aws_subnet.public-eu-west-3a.id,
+    aws_subnet.public-eu-west-3b.id
+  ]
+}
+
 output "vpc_id" {
-  description = "The ID of the VPC."
-  value       = aws_vpc.this.id
-}
-
-output "private_subnet_a_id" {
-  description = "The ID of the private subnet A."
-  value       = aws_subnet.private_subnet_a.id
-}
-
-output "private_subnet_b_id" {
-  description = "The ID of the private subnet B."
-  value       = aws_subnet.private_subnet_b.id
-}
-
-output "public_subnet_a_id" {
-  description = "The ID of the public subnet A."
-  value       = aws_subnet.public_subnet_a.id
-}
-
-output "public_subnet_b_id" {
-  description = "The ID of the public subnet B."
-  value       = aws_subnet.public_subnet_b.id
-}
-
-output "nat_gateway_id" {
-  description = "The ID of the NAT Gateway."
-  value       = aws_nat_gateway.this.id
-}
-
-output "internet_gateway_id" {
-  description = "The ID of the Internet Gateway."
-  value       = aws_internet_gateway.this.id
-}
-
-output "eks_security_group_id" {
-  description = "The security group ID for EKS."
-  value       = aws_security_group.eks_sg.id
-}
-
-output "rds_security_group_id" {
-  description = "The security group ID for RDS."
-  value       = aws_security_group.rds_sg.id
-}
-
-output "rds_subnet_group_name" {
-  description = "The name of the DB subnet group."
-  value       = aws_db_subnet_group.my_db_subnet_group.name
+  value = aws_vpc.main.id
 }
