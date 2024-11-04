cbc36775401d1fdc03b8a16a07dae0a63c90f4d5
Roman Strypa <romanstripa@gmail.com>
2024-11-04 13:51:20 +0000

    new terraform main&eks varfile

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -89,7 +89,7 @@ resource "aws_nat_gateway" "main" {
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
-  route = {
+  route {
     cidr_block        = "0.0.0.0/0"
     nat_gateway_id    = aws_nat_gateway.main.id 
   }
@@ -102,7 +102,7 @@ resource "aws_route_table" "private" {
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
-  route = {
+  route {
     cidr_block        = "0.0.0.0/0"
     nat_gateway_id    = aws_internet_gateway.main.id 
   }
