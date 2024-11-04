65ddcc8ac15972b5e67537f6a85d3019fe28d9bd
Roman Strypa <romanstripa@gmail.com>
2024-11-04 13:55:05 +0000

    new terraform main&eks varfile

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -37,10 +37,10 @@ resource "aws_db_instance" "greencity" {
   db_name                 = var.db_name
   username                = var.username
   password                = var.password
-  vpc_security_group_ids  = aws_db_subnet_group.main.name
-  db_subnet_group_name    = [aws_security_group.main.id]
-  publicly_accessible = false
-  skip_final_snapshot = true
+  db_subnet_group_name    = aws_db_subnet_group.main.name
+  vpc_security_group_ids  = [aws_security_group.main.id]
+  publicly_accessible     = false
+  skip_final_snapshot     = true
   
   tags = {
     Name = "${var.name}-instance"
