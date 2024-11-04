1368d70af765873cb6c48a07381236b58b68d1f6
Roman Strypa <romanstripa@gmail.com>
2024-11-04 13:11:51 +0000

    new terraform main&eks varfile

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,30 +1,37 @@
 module "vpc" {
-  source             = "./modules/vpc"
-  cluster_name       = var.eks.cluster_name
+  source         = "./modules/vpc"
+  region         = var.region
+  vpc_cidr_block = var.vpc_cidr_block
+  cluster_name   = var.cluster_name
 }
 
 module "rds" {
-  source             = "./modules/rds"
-  name               = var.rds.name
-  subnet_ids         = module.vpc.private_subnet_ids
-  vpc_id             = module.vpc.vpc_id
-  allocated_storage  = var.rds.allocated_storage
-  engine             = var.rds.engine
-  engine_version     = var.rds.engine_version
-  instance_class     = var.rds.instance_class
-  db_name            = var.rds.db_name
-  username           = var.username  
-  password           = var.password 
+  source            = "./modules/rds"
+  name              = var.rds.name
+  subnet_ids        = module.vpc.private_subnet_ids
+  vpc_id            = module.vpc.vpc_id
+  allocated_storage = var.rds.allocated_storage
+  engine            = var.rds.engine
+  engine_version    = var.rds.engine_version
+  instance_class    = var.rds.instance_class
+  db_name           = var.rds.db_name
+  username          = var.username  
+  password          = var.password 
 
   depends_on = [module.vpc]
 }
 
 module "eks" {
   source             = "./modules/eks"
-  cluster_sg_name    = "${var.eks.cluster_name}-cluster-sg"
-  node_group_name    = "${var.eks.cluster_name}-node-group"
-  eks_node_sg_name   = "${var.eks.cluster_name}-node-sg"
-  cluster_name       = var.eks.cluster_name
+  vpc_id             = module.vpc.vpc_id
+  cluster_sg_name    = "${var.cluster_name}-cluster-sg"
+  nodes_sg_name      = "${var.cluster_name}-node-sg"
+  node_group_name    = "${var.cluster_name}-node-group"
+  private_subnet_ids = module.vpc.private_subnet_ids
+  publc_subnet_ids   = module.vpc.publc_subnet_ids
+  cluster_name       = var.cluster_name 
+
+  depends_on = [module.vpc]
 }
 
 variable "username" {
